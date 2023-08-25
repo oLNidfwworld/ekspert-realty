0b24f6a13674fb95e62543b32fc2869b0c04ca9d
oLNidfwworld <kuvetov@bk.ru>
2023-08-25 16:57:22 +0300

    Сделать аля fancybox

---
+++ components/GallertView/Thumbnail.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+const props = defineEmits({
+  currentSlide : Number
+})
+const emits = defineEmits(['thumbs','update:currentSlide'])
+
+const onSwiper = (swiper) => {
+  emits('thumbs', swiper)
+}
+</script>
+
+<template>
+  <Swiper @swiper="onSwiper" :slides-per-view="10" :space-between="50" :modules="[Thumbs]" watch-slides-progress>
+    <SwiperSlide v-for="n in 20" :key="n" @click.stop="$emit('update:currentSlide', Number(n) - 1)">
+      <img :src="'https://picsum.photos/id/'+n+'/640/480'" style="height: 100px" />
+    </SwiperSlide>
+  </Swiper>
+</template>
