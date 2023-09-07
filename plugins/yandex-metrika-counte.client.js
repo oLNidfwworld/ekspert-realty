1579a1d18ee6feb31d72a5133072c8f9d98a9ec2
oLNidfwworld <kuvetov@bk.ru>
2023-09-07 10:02:20 +0300

    add ym metrika

---
+++ plugins/yandex-metrika-counte.client.js
@@ -0,0 +1,23 @@
+export default defineNuxtPlugin(  app   => {
+
+    if (process.env.NODE_ENV !== 'production') return;
+ 
+
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(94861471, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true,
+         webvisor:true
+    });
+ 
+  
+    app.router.afterEach((to, from) => {
+      ym(94861471, 'hit', to.fullPath);
+    })
+  }) 
