fe5e0cecd35298a6665da23d03c9ab564c320f90
oLNidfwworld <kuvetov@bk.ru>
2023-09-11 12:56:46 +0300

    edit cors

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler((event) => {
     const headers = {
-        'Access-Control-Allow-Origin': 'http://api-ekspert.tw1.ru/',
-        'crossOriginResourcePolicy': 'http://api-ekspert.tw1.ru/',
-        'crossOriginOpenerPolicy': 'http://api-ekspert.tw1.ru/',
+        'Access-Control-Allow-Origin': 'https://api-ekspert.tw1.ru/',
+        'crossOriginResourcePolicy': 'https://api-ekspert.tw1.ru/',
+        'crossOriginOpenerPolicy': 'https://api-ekspert.tw1.ru/',
         'crossOriginEmbedderPolicy': 'require-corp',
         'contentSecurityPolicy': "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
         'X-XSS-Protection': 1
