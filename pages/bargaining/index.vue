47992a1c6a2a80eb0bf573bc670e8bbdd095b553
oLNidfwworld <kuvetov@bk.ru>
2023-10-09 00:13:37 +0300

    много говнокода и суперрефакторить, увы и ах этим я заниматься не буду

--- pages/bargaining/index.vue
+++ pages/bargaining/index.vue
@@ -1,26 +1,35 @@
-<script setup>
-import { YandexMap } from "vue-yandex-maps"; 
+<script setup lang="ts">
+import { YandexMap } from "vue-yandex-maps";
 import EBtn from "~/components/Base/E-btn.vue";
 import EInput from "~/components/Base/E-input.vue";
 import ETextarea from "../../components/Base/E-textarea.vue";
+import {watch} from "vue";
+import {useSeoMeta} from "unhead";
 
 definePageMeta({
     layout: "bargaining",
 })
-const mapCenter = ref([55.77157346148135, 38.5848870091816, ]) 
-const zoom = 17; 
-const castilb = ref(null);
+
+
+useSeoMeta(
+    {
+      title : 'Продажа участков в Субботино'
+    }
+)
+
+const mapCenter = ref([55.77157346148135, 38.5848870091816, ])
+const zoom = 17;
 const { data: placements, pending, error, refresh } = await useAsyncData(
     () => useApiFetch(`/Bargaining/`)
 );
-const { data: foregroundLayout } = await useFetch('/api/foreground'); 
- 
-const mapInit = (mapHandler) => { 
+const { data: foregroundLayout } = await useFetch('/api/foreground');
 
-    var objectManager = new ymaps.ObjectManager({}); 
+const mapInit = (mapHandler) => {
+
+    var objectManager = new ymaps.ObjectManager({});
     mapHandler.geoObjects.add(objectManager);
-    objectManager.add(toRaw(foregroundLayout.value)); 
-    toRaw(placements.value).items.map(item => {  
+    objectManager.add(toRaw(foregroundLayout.value));
+    toRaw(placements.value).items.map(item => {
         let fillCol = ';'
         if(item.STATUS.CODE === 'SELLABLE'){
             fillCol = '#79cd79';
@@ -44,8 +53,8 @@ const mapInit = (mapHandler) => {
                 strokeWidth: 1,
             },
             properties: {
-                hintContent: `Участок №${item.NAME}`, // Подсказка при наведении на полигон 
-                balloonContent: balloonContentTemplate(item.NAME, item.SQUARE, item.STATUS, item.PRICE)// Содержимое балуна 
+                hintContent: `Участок №${item.NAME}`, // Подсказка при наведении на полигон
+                balloonContent: balloonContentTemplate(item.NAME, item.SQUARE, item.STATUS, item.PRICE)// Содержимое балуна
             }
         });
 
@@ -53,32 +62,40 @@ const mapInit = (mapHandler) => {
             (item.PLACE.reduce((a,b) => a + b[0], 0) / (item.PLACE.length )),
             (item.PLACE.reduce((a,b) => a + b[1], 0) / (item.PLACE.length ))
         ];
-        var textPlacemark = new ymaps.Placemark(center, { 
+        var textPlacemark = new ymaps.Placemark(center, {
             iconContent: `<span class="polygon-text">${item.NAME}</span>` // Текст маркера
         }, {
             iconLayout: 'default#imageWithContent',
-            iconImageHref: '/nil.png', 
+            iconImageHref: '/nil.png',
             iconOffset: [-10, -5],
             iconImageSize: [1, 1] // Размеры иконки (ширина, высота)
         });
         mapHandler.geoObjects.add(textPlacemark);
-    }) 
+    })
 
     objectManager.objects.events.add('click', function (e) {
             var objectId = e.get('objectId');
-            objectManager.objects.balloon.open(objectId); 
-            castilb.value = objectId;
-        });  
+            objectManager.objects.balloon.open(objectId);
+            console.log()
+        });
+    objectManager.objects.events.add('balloonopen', () => {
+      let btnOrder = document.querySelector('.ballon button')
+      if(btnOrder){
+        btnOrder.addEventListener('click',()=>{
+          makeOrder(btnOrder.dataset.id);
+        })
+      }
+    })
     objectManager.add(toRaw(foregroundLayout.value));
 
     mapHandler.events.add('boundschange', function (event) {
         var newZoom = event.get('newZoom');
-        var allGeoObjects = mapHandler.geoObjects; 
+        var allGeoObjects = mapHandler.geoObjects;
         if(newZoom < 18){
             allGeoObjects.each(  function(geoObject) {
                 if (geoObject instanceof ymaps.Placemark) {
                     geoObject.options.set('visible', false);
-                } 
+                }
             });
         }else{
             allGeoObjects.each(  function(geoObject) {
@@ -87,43 +104,102 @@ const mapInit = (mapHandler) => {
                 }
             });
         }
-    }); 
-} 
-const balloonContentTemplate = (name, square, stat, price) => {
-    return `
-    
-    <div class="ballon" >
-    
-        <div class="ballon__wrapper"> 
-
-            <div class="ballon__title">Участок номер ${name}</div> 
-            <ul class="ballon__list">
-                <li><span>Стоимость :</span><span>${price}</span></li>
-                <li><span>Площадь :</span><span>${square}м<sup>2</sup></span></li>
-                <li><span>Статус :</span><span class="stat_${stat.CODE}">${stat.NAME}</span></li>
-            </ul> 
-            <button   data-id="${name}" class="btn border-red text-red p-3 mt-3 btn-red">Заказать</button>
-        </div>
-
-    </div>
-    
-    `
+    });
 }
-  
-watch(()=>castilb.value,()=>{ 
-    console.log(castilb.value)
-    document.querySelector(`button[data-id="${castilb.value}"]`).addEventListener('click',()=>{
-                    console.log('ddd')
-                })
-})
+const balloonContentTemplate = (name, square,stat , price) => {
+    if(stat.CODE === 'SELLED'){
+      return `
+
+      <div class="ballon" >
+
+          <div class="ballon__wrapper">
+
+              <div class="ballon__title">Участок номер ${name}</div>
+              <ul class="ballon__list">
+                  <li><span>Стоимость :</span><span>${price}</span></li>
+                  <li><span>Площадь :</span><span>${square}м<sup>2</sup></span></li>
+                  <li><span>Статус :</span><span class="stat_${stat.CODE}">${stat.NAME}</span></li>
+              </ul>
+          </div>
+
+      </div>
+
+      `
+    }else{
+      return `
+
+      <div class="ballon" >
+
+          <div class="ballon__wrapper">
+
+              <div class="ballon__title">Участок номер ${name}</div>
+              <ul class="ballon__list">
+                  <li><span>Стоимость :</span><span>${price}</span></li>
+                  <li><span>Площадь :</span><span>${square}м<sup>2</sup></span></li>
+                  <li><span>Статус :</span><span class="stat_${stat.CODE}">${stat.NAME}</span></li>
+              </ul>
+              <button   data-id="${name}" class="btn border-red text-red p-3 mt-3 btn-red">Заказать</button>
+          </div>
+
+      </div>
+
+      `
+    }
+
+}
+
+const whatsObject = ref(undefined);
+
+const userName = ref(''), userPhone = ref(''), userComment = ref(''), userEmail = ref('')
 
 const fallback = ref(false), responseMsg = ref('');
 const isPopupVisible = ref(false);
+const validation = ref(false);
+const validationMessage = ref('');
+
+const makeOrder =  (id : String) => {
+  const {NAME : placementName} = placements.value.items.find(x => x.NAME == id);
+  whatsObject.value = placementName;
+  isPopupVisible.value = true;
+}
+const sendData = async (e) => {
+
+  if(userPhone.value.length < 17){
+    validationMessage.value = 'Неверный формат номера телефона'
+    validation.value = false;
+  }else{
+    validation.value = true;
+    validationMessage.value = ''
+  }
+
+  if (validation.value) {
+    await useApiFetchWithRefresh('/Mail/', {
+      method: 'POST',
+      params: {
+        action: 'NEW_BARG',
+        placement: whatsObject.value,
+        phone: userPhone.value,
+        name: userName.value,
+        comment : userComment.value,
+        email : userEmail.value
+      }
+    }).then((e)=>{
+      if(e.data.value.status){
+        responseMsg.value = e.data.value.msg
+        fallback.value = true;
+      }
+    })
+  }
+
+}
+watch(()=>userPhone,()=>{
+  console.log(userPhone.value)
+})
 </script>
 <template>
-    <ClientOnly> 
+    <ClientOnly>
         <div class="relative">
-            <YandexMap @created="mapInit" :coordinates="mapCenter" :zoom="zoom"> 
+            <YandexMap @created="mapInit" :controls="[]" :coordinates="mapCenter" :zoom="zoom">
             </YandexMap>
             <div class="history">
                 <h2 class="text-[20px] font-bold mb-[15px]">Условные обозначения</h2>
@@ -133,20 +209,21 @@ const isPopupVisible = ref(false);
                     <li><span class="history__col"  style="background:var(--sellable);"></span> В продаже</li>
                 </ul>
             </div>
+            <h1 class="title auto-place">Продажа участков в Субботино</h1>
         </div>
     </ClientOnly>
 
     <e-popup-form :fallback-income="fallback"  :is-visible="isPopupVisible" @fallback-return="fallback=false" @close="isPopupVisible = !isPopupVisible" >
             <template v-slot:header>
-                <h3 class="">Оставить заявку</h3>
+                <h3 class="">Заказать участок {{ whatsObject }}</h3>
             </template>
             <template v-slot:content>
                 <form @submit.prevent="sendData($event)">
                     <div class="grid gap-4 mb-4">
-                        <e-input v-model="name" required placeholder="Имя*"></e-input>
-                        <e-input mask="+{7} {9}00 000-00-00" type="phone" v-model="phone" required placeholder="Телефон*"></e-input>
-                        <e-input type="email" v-model="email" placeholder="Email"></e-input>
-                        <ETextarea style="resize: none;" rows="5" v-model="comment" placeholder="Комментарий"/>
+                        <e-input v-model="userName" required placeholder="Имя*"></e-input>
+                        <e-input mask="+{7} {9}00 000-00-00" type="phone" v-model="userPhone" required placeholder="Телефон*"></e-input>
+                        <e-input type="email" v-model="userEmail" placeholder="Email"></e-input>
+                        <ETextarea style="resize: none;" rows="5" v-model="userComment" placeholder="Комментарий"/>
                     </div>
                     <div class="text-red text-[14px] mb-3">
                         {{ validationMessage }}
@@ -170,14 +247,17 @@ const isPopupVisible = ref(false);
 #map {
     height: 80vh !important;
 }
+.auto-place{
+  @apply absolute text-[16px] sm:text-[24px] md:text-[30px] z-[3] top-5 left-0 right-0 m-auto w-fit bg-white py-2 px-4;
+}
 .history{
-    @apply absolute z-[9999] left-[40px] bottom-[40px] bg-white p-5;
+    @apply absolute z-[3] sm:left-[40px] bottom-[40px] bg-white p-5 left-0 right-0 sm:right-[unset] ;
             border: 1px solid #6f7985;
     &__wrapper{
         & li{
             @apply flex gap-4 mb-2;
         }
-        
+
     }
         &__col{
             @apply block w-[70px] h-[20px];
